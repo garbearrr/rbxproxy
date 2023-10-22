602ee1ae341a4a38b69ef8c6c42a9e495c295c76
garbearrr <garretthweaver2@gmail.com>
2023-10-21 22:00:05 -0400

    typing fix

--- dist/mod.d.ts
+++ dist/mod.d.ts
@@ -1,5 +1,5 @@
 export declare function Proxy<T extends {}>(target: T, handler?: ProxyHandler<T>): T;
 export interface ProxyHandler<T extends {}> {
-    get?: (obj: T, key: keyof T, accessor?: (key: keyof T) => T[keyof T]) => any;
+    get?: (obj: T, key: keyof T, accessor: (key: keyof T) => T[keyof T]) => any;
     set?: (obj: T, key: keyof T, value: any) => any;
 }
--- src/mod.ts
+++ src/mod.ts
@@ -35,6 +35,6 @@ export function Proxy<T extends {}>(target: T, handler: ProxyHandler<T> = {}): T
 }
 
 export interface ProxyHandler<T extends {}> {
-    get?: (obj: T, key: keyof T, accessor?: (key: keyof T) => T[keyof T]) => any;
+    get?: (obj: T, key: keyof T, accessor: (key: keyof T) => T[keyof T]) => any;
     set?: (obj: T, key: keyof T, value: any) => any;
 }
