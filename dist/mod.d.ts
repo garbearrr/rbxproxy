c7869afd3dbee7c6044c8d1d3187332c38f216a2
garbearrr <garretthweaver2@gmail.com>
2024-01-17 15:38:54 -0500

    initial commit

--- dist/mod.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function Proxy<T extends {}>(target: T, handler?: ProxyHandler<T>): T;
-export interface ProxyHandler<T extends {}> {
-    get?: (obj: T, key: keyof T, accessor: (key: keyof T) => T[keyof T]) => any;
-    set?: (obj: T, key: keyof T, value: any) => any;
-}
